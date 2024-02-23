5b2514b2047728beabf607a0ee7247f939c9f243
David Angulo <dfangulo@gmail.com>
2024-02-23 08:56:23 -0700

    Los menus funcionando al 100%, ahroa agregar las funciones para comenzar a utilizar el simulador..

--- app/menu.py
+++ app/menu.py
@@ -1,41 +1,57 @@
 from os import system
-from .menu_items import menu_options
 from .page import Page
+from app.network.dhcp_server import DHCP, MacAddress
+
 
 
 class Menu:
     def __init__(self) -> None:
-        self.menu_options: dict = menu_options
         self.load_pages()
-        self.current_page: str = 'main_menu'
+        self.current_page: object = self.main_page
+        self.before_page: object = None
 
     def load_pages(self) -> None:
         self.main_page = Page(
-            name='main_menu',
+            func_name=None,
+            menu_name="main_page",
             head="Simulador de Redes",
-            options=["Nueva Red", "Cargar Red"],
+            options_display=["Nueva Red", "Cargar Red"],
+            options={
+                "1": "new_network_page",
+                "2": "load_network_page",
+                "q": "exit",
+            },
             bottom="(q) Cerrar aplicación",
         )
-        self.new_network = Page(
-            name="sub_menu_1",
+        self.new_network_page = Page(
+            func_name=None,
+            menu_name="sub_menu_1",
             head="Nueva Red",
-            options=[
-                "Menu_1_add_Switch",
+            options_display=[
+                "Test Network",
                 "Menu_1_add_DHCP",
                 "Menu_1_add_device",
                 "Menu_1_remove_device",
             ],
+            options={
+                "1": "add_switch",
+                "r": "return",
+            },
             bottom="(r) Regresar",
         )
-        self.new_network = Page(
-            name="sub_menu_2",
+        self.load_network_page = Page(
+            func_name=None,
+            menu_name="sub_menu_2",
             head="Cargar Red",
-            options=[
+            options_display=[
                 "Menu_1_add_Switch",
                 "Menu_1_add_DHCP",
                 "Menu_1_add_device",
                 "Menu_1_remove_device",
             ],
+            options={
+                "r": "return",
+            },
             bottom="(r) Regresar",
         )
 
@@ -50,11 +66,11 @@ class Menu:
     def input_menu(self) -> str:
         while True:
             system("cls")
-            self.main_page.display()
+            self.current_page.display()
             answer = input("Seleciona una opçión: ").strip().lower()
             if len(answer) > 0:
-                if answer in self.menu_options.get(self.current_page):
-                    return self.menu_options[self.current_page][answer]
+                if answer in self.current_page.options:
+                    return self.current_page.options.get(answer)
                 else:
                     """
                     Mostrar la información de las llaves en self.page_menu
@@ -64,13 +80,68 @@ class Menu:
                         f"La opcion:'{answer}', no es valida",
                         "\nSeleciona alguna opcion valida:",
                     )
-                    for key in self.menu_options[self.current_page]:
+                    for key in self.current_page.options:
                         keys.append(key)
                     print("\t", ", ".join(keys))
                     input("Enter para continuar!")
 
     def update_page_menu(self, page_menu: str) -> None:
-        self.current_page = page_menu
+        page = getattr(self, page_menu, None)
+        if isinstance(page, Page):
+            self.before_page = self.current_page
+            self.current_page = page
+        else:
+            print(f"No se pudo encontrar la página '{page_menu}'.")
 
-    def sub_menu_1(self):
-        self.update_page_menu("sub_menu_1")
+    def add_switch(self) ->None:
+        try:
+            # Ejemplo de uso
+            ip = [192, 168, 1, 1]
+            netmask = 30
+            ip_start = [192, 168, 1, 20]
+            ip_end = [192, 168, 1, 50]
+            gateway = [192, 168, 1, 7]
+            dns1 = [192, 168, 1, 1]
+            dns2 = [80, 80, 81, 81]
+
+            dhcp_server = DHCP(
+                network_ip=ip,
+                subnet_mask=netmask,
+                ip_range_start=ip_start,
+                ip_range_end=ip_end,
+                gateway=gateway,
+                dns1=dns1,
+                dns2=dns2,
+            )
+
+            # Simular una solicitud de IP para una nueva máquina con una dirección MAC específica
+            mac_address = MacAddress("00:11:22:33:44:55")
+            mac_address1 = MacAddress()
+            print(mac_address)
+            ip_address = dhcp_server.request_ip(str(mac_address))
+            ip_address1 = dhcp_server.request_ip(str(mac_address1))
+            print(dhcp_server)
+            print(
+                "Dirección IP asignada a la máquina con MAC",
+                mac_address,
+                ":",
+                ip_address,
+                dhcp_server.subnet_mask,
+                dhcp_server.gateway,
+                dhcp_server.dns1,
+                dhcp_server.dns2,
+            )
+            print(
+                "Dirección IP asignada a la máquina con MAC",
+                mac_address1,
+                ":",
+                ip_address1,
+                dhcp_server.subnet_mask,
+                dhcp_server.gateway,
+                dhcp_server.dns1,
+                dhcp_server.dns2,
+            )
+            print(dhcp_server.leased_ips)
+            input("Enter Para continuar!.")
+        except ValueError as e:
+            print("Error:", e)
--- app/page.py
+++ app/page.py
@@ -1,8 +1,18 @@
 class Page:
-    def __init__(self, name: str, head: str, options: list, bottom: str) -> None:
-        self.name = name
+    def __init__(
+        self,
+        menu_name: str,
+        func_name: str,
+        head: str,
+        options: dict,
+        options_display: list,
+        bottom: str,
+    ) -> None:
+        self.menu_name = menu_name
+        self.func_name = func_name
         self.head = head
         self.options = options
+        self.options_display = options_display
         self.bottom = bottom
         self.large_page = 27
 
@@ -14,44 +24,10 @@ class Page:
         print(f"{self.head.center(self.large_page)}")
         print("-" * self.large_page, 2 * "\n")
 
-        for index, option in enumerate(self.options, start=1):
+        for index, option in enumerate(self.options_display, start=1):
             print(f"\t{index}. {option.center(len(option))}")
 
         print(2 * "\n", "-" * self.large_page)
         print(f"{self.bottom.center(self.large_page)}")
         print("-" * self.large_page)
         print("\n")
-
-
-if __name__ == "__main__":
-    # Crear las páginas
-    pagina1 = Page(
-        head="Simulador de Redes",
-        options=["Menu 1", "Menu 2", "Menu 3", "Menu 4"],
-        bottom="Cerrar aplicación",
-    )
-    pagina2 = Page(
-        head="Otra página",
-        options=["Opción 1", "Opción 2"],
-        bottom="Volver al menú principal",
-    )
-
-    # Mostrar la primera página
-    pagina_actual = pagina1
-
-    while True:
-        pagina_actual.display()
-        opcion = input("Seleccione una opción:").strip().lower()
-
-        if opcion == "q":
-            print("Saliendo...")
-            break
-        elif opcion.isdigit():
-            opcion = int(opcion)
-            if 1 <= opcion <= len(pagina_actual.options):
-                print(f"Seleccionaste la opción {pagina_actual.options[opcion - 1]}")
-                # Aquí podrías implementar la lógica para manejar la opción seleccionada
-            else:
-                print("Opción inválida. Por favor, selecciona una opción válida.")
-        else:
-            print("Opción inválida. Por favor, selecciona una opción válida.")
